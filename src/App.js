973063ec0578bb85bd46435b866619057d96f913
Sarthak Bansal <sbansal793@gmail.com>
2021-09-20 14:38:21 +0530

    installed packages and resolved card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import Navbar from './navbar';
-import Cover from './cover';
-import LoadingImage from './Loading';
-import  { useState } from 'react';
+import Navbar from "./navbar";
+import Cover from "./cover";
+import LoadingImage from "./Loading";
+import { useState } from "react";
 import "./page.css";
+import { Route, Redirect, Switch } from "react-router-dom";
+
 
 function App() {
-  const [isShow,setIsShow]=useState(false);
+  const [isShow, setIsShow] = useState(false);
 
-
-  function LoadingScreenState(){
+  function LoadingScreenState() {
     setIsShow(true);
   }
 
-
   return (
     <div className="selection">
-    <LoadingImage Show={isShow} />
-    <Navbar />
-    <Cover LoadEvent={LoadingScreenState} Show={isShow}/>
+      <LoadingImage Show={isShow} />
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Cover LoadEvent={LoadingScreenState} Show={isShow} />
+          
+        </Route>
+        <Route path="/home" exact>
+          <Redirect to="/" />
+        </Route>
+        <Route path="/artisans" exact>
+          <Redirect to="/" />
+        </Route>
+        <Route path="/products" exact>
+          <Redirect to="/" />
+        </Route>
+        <Route path="/story" exact>
+          <Redirect to="/" />
+        </Route>
+        <Route path="/support" exact>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,10 @@
-import react from "react";
 import "./styles.css";
 
 const CardArtist = (props) => {
   return (
     <div className="cardArtist cardArtistImage">
-      <div class="borderArtist">
-        <h2 className="h2Artist">{props.name}</h2>
+      <div className="borderArtist">
+        <h2 className="h2Artist">Al Pacino</h2>
       </div>
     </div>
   );
