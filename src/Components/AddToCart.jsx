e9b83b1b311289f769b162ca697c05591a92d3cd
Sarthak Bansal <sbansal793@gmail.com>
2021-09-21 15:57:21 +0530

    added add to cart and delete

---
+++ src/Components/AddToCart.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useRef } from "react";
+import "./AddToCart.scss";
+
+
+function AddToCart() {
+  const buttonRef = useRef();
+  const [addedState, setAddedState]=useState(false);
+
+
+  const ClickButton = (event) => {
+    event.preventDefault();
+    setAddedState(true);
+    setTimeout(() => {
+        setAddedState(false);
+      }, 3500);
+  };
+
+
+  return (
+    <button ref={buttonRef} onClick={ClickButton} className={`add-to-cart ${addedState && 'added'}`}>
+      <div className="default">Add to cart</div>
+      <div className="success">Added</div>
+      <div className="cart">
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div className="dots"></div>
+    </button>
+  );
+}
+
+export default AddToCart;
