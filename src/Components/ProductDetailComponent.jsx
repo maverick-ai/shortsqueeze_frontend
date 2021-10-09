f74afbfadac998b0215bc804b78b00ca363bbfc0
Sarthak Bansal <sbansal793@gmail.com>
2021-10-09 17:23:34 +0530

    added dropdown

--- src/Components/ProductDetailComponent.jsx
+++ src/Components/ProductDetailComponent.jsx
@@ -6,7 +6,9 @@ import AddToCart from "./AddToCart";
 import serverBaseURL from "../constants";
 import { cartActions } from "../Store/AddToCartSlice";
 import { useDispatch } from "react-redux";
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from "react-bootstrap/Carousel";
+import Dropdown from "react-bootstrap/Dropdown";
+import Rating from "./Rating";
 
 function ProductDetailComponent(props) {
   const dispatch = useDispatch();
@@ -16,6 +18,12 @@ function ProductDetailComponent(props) {
     dispatch(cartActions.increaseItem({ id: props.key, price: props.price }));
   }
 
+  function dropdownOnSelect(eventKey, event) {
+    console.log("dropdown ...........");
+    console.log(eventKey);
+    console.log("dropdown ...........");
+    console.log(event.target);
+  }
   // useEffect(() => {
   //   console.log("inside list .................");
   //   console.log(props.ItemNumber);
@@ -67,73 +75,44 @@ function ProductDetailComponent(props) {
   //     }
   //   }
   // }, []);
- 
-  
+
   return (
     <div className="container-lg paddingTop">
       <div className="row">
-        <div className="col-sm-6">
-          <Carousel indicators={true} pause='hover'>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src= {process.env.PUBLIC_URL + "/testProduct.jpg"}
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
+        <div className="col-lg-6">
+          <Carousel indicators={true} pause="hover">
+            <Carousel.Item key={1}>
               <img
                 className="d-block w-100 carouselImage"
-                src={
-                  process.env.PUBLIC_URL + "/cover.jpg"
-                }
+                src={process.env.PUBLIC_URL + "/testProduct.jpg"}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item key={2}>
+              <img
+                className="d-block w-100 carouselImage"
+                src={process.env.PUBLIC_URL + "/cover.jpg"}
                 alt="Second slide"
               />
-
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item key={3}>
               <img
-                className="d-block w-100"
+                className="d-block w-100 carouselImage"
                 src={process.env.PUBLIC_URL + "/testProduct.jpg"}
                 alt="Third slide"
               />
-
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item key={4}>
               <img
-                className="d-block w-100"
-                src={
-                  process.env.PUBLIC_URL + "/cover.jpg"
-                }
+                className="d-block w-100 carouselImage"
+                src={process.env.PUBLIC_URL + "/cover.jpg"}
                 alt="Second slide"
               />
-
-              <Carousel.Caption>
-                <h3>fourth slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
         </div>
-        <div className="col-sm-6 ">
-          <div>
+        <div className="col-lg-6 align-self-center">
+          <div className="paddingForTitle">
             <h2 className="title">{props.title} (NFT + Real Art)</h2>
             <h4 className="price">
               <span className="priceText">Price:</span>{" "}
@@ -147,11 +126,28 @@ function ProductDetailComponent(props) {
               </span>{" "}
               <span className="currencyType">(Crypto currency)</span>
             </h4>
+            <Dropdown onSelect={dropdownOnSelect} className="d-inline mx-2">
+              <Dropdown.Toggle id="dropdown-autoclose-true">
+                Default Dropdown
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey="option-1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="option-2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="option-3">3</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <AddToCart ClickAdd={AddToCartSlice} />
           </div>
-          <AddToCart ClickAdd={AddToCartSlice} />
         </div>
       </div>
       <p className="info">{props.description}</p>
+      <div className=" d-flex justify-content-center">
+        <h4 className="ratingHeading">Rate</h4>
+      </div>
+      <div className="ratingPadding d-flex justify-content-center">
+        <Rating />
+      </div>
     </div>
   );
 }
