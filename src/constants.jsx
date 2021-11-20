1b8e352b40f8298bed81f9767fb240487ce3927c
Sarthak Bansal <sbansal793@gmail.com>
2021-11-20 15:53:47 +0530

    made changes to url for the deployed backend

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,18 +1,35 @@
-const serverBaseURL = "http://127.0.0.1:8000/";
-const serverBaseURLImage = "http://127.0.0.1:8000";
-const LogInURL = "http://127.0.0.1:8000/api/login/";
-const ListProductsURL = "http://127.0.0.1:8000/api/listproducts/";
-const Host = "127.0.0.1:8000";
+// const serverBaseURL = "http://127.0.0.1:8000/";
+// const serverBaseURLImage = "http://127.0.0.1:8000";
+// const LogInURL = "http://127.0.0.1:8000/api/login/";
+// const ListProductsURL = "http://127.0.0.1:8000/api/listproducts/";
+// const Host = "127.0.0.1:8000";
+// const shortsqueezeETHAccount = "0x96C5A9bA429265101638c5436E8cF36eA2500f5A";
+// const ProfileURL = "http://127.0.0.1:8000/api/user/";
+// const ProductDetailBaseURL = "http://127.0.0.1:8000/api/product/";
+// const OrderDetailBaseURL = "http://127.0.0.1:8000/api/order/";
+// const MyOrderURL="http://127.0.0.1:8000/api/myorders/";
+// const ListArtistURL="http://127.0.0.1:8000/api/listartist/";
+// const SupportTicketURL="http://127.0.0.1:8000/api/create_support_ticket/";
+// const IPInfoURL = "https://ipapi.co/json/";
+// const OutOfStockURL="http://127.0.0.1:8000/api/out_of_stock/";
+// const RegistrationURL="http://127.0.0.1:8000/api/registration/";
+
+
+const serverBaseURL = "https://backend.shortsqueeze.space/";
+const serverBaseURLImage = "https://backend.shortsqueeze.space";
+const LogInURL = "https://backend.shortsqueeze.space/api/login/";
+const ListProductsURL = "https://backend.shortsqueeze.space/api/listproducts/";
+const Host = "backend.shortsqueeze.space";
 const shortsqueezeETHAccount = "0x96C5A9bA429265101638c5436E8cF36eA2500f5A";
-const ProfileURL = "http://127.0.0.1:8000/api/user/";
-const ProductDetailBaseURL = "http://127.0.0.1:8000/api/product/";
-const OrderDetailBaseURL = "http://127.0.0.1:8000/api/order/";
-const MyOrderURL="http://127.0.0.1:8000/api/myorders/";
-const ListArtistURL="http://127.0.0.1:8000/api/listartist/";
-const SupportTicketURL="http://127.0.0.1:8000/api/create_support_ticket/";
+const ProfileURL = "https://backend.shortsqueeze.space/api/user/";
+const ProductDetailBaseURL = "https://backend.shortsqueeze.space/api/product/";
+const OrderDetailBaseURL = "https://backend.shortsqueeze.space/api/order/";
+const MyOrderURL="https://backend.shortsqueeze.space/api/myorders/";
+const ListArtistURL="https://backend.shortsqueeze.space/api/listartist/";
+const SupportTicketURL="https://backend.shortsqueeze.space/api/create_support_ticket/";
 const IPInfoURL = "https://ipapi.co/json/";
-const OutOfStockURL="http://127.0.0.1:8000/api/out_of_stock/";
-const RegistrationURL="http://127.0.0.1:8000/api/registration/";
+const OutOfStockURL="https://backend.shortsqueeze.space/api/out_of_stock/";
+const RegistrationURL="https://backend.shortsqueeze.space/api/registration/";
 
 
 export {
